14b5a66cfe5e6d91efb61e90fc997520caa40ccd
Uday <udayreddy237093@gmail.com>
2024-12-06 08:11:28 +0530

    build updates

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,8 +1,12 @@
-def call(String dockerHubUsername, String imageName) {
+def call(String dockerHubUsername, String imageName, String dockerfilePath = '.') {
     // Build the Docker image
-    sh "docker build --build-arg REACT_APP_RAPID_API_KEY=59be1b8a0bmsh2df895722292ad8p1366aajsn4d7893fbcde8 -t ${imageName} ."
-     // Tag the Docker image
+    sh """
+    docker build --build-arg REACT_APP_RAPID_API_KEY=59be1b8a0bmsh2df895722292ad8p1366aajsn4d7893fbcde8 \
+                 -t ${imageName} -f ${dockerfilePath}/Dockerfile ${dockerfilePath}
+    """
+    // Tag the Docker image
     sh "docker tag ${imageName} ${dockerHubUsername}/${imageName}:latest"
+
     // Push the Docker image
     withDockerRegistry([url: 'https://index.docker.io/v1/', credentialsId: 'docker']) {
         sh "docker push ${dockerHubUsername}/${imageName}:latest"
