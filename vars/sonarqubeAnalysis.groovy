425dc8bab2f1ecd956ceb4dab8392a86e30e3b82
udayreddy0 <141111269+udayreddy0@users.noreply.github.com>
2025-01-29 12:00:06 +0530

    Update sonarqubeAnalysis.groovy

--- vars/sonarqubeAnalysis.groovy
+++ vars/sonarqubeAnalysis.groovy
@@ -2,10 +2,10 @@ def call() {
     withSonarQubeEnv('sonar-server') {
         sh '''
             $SCANNER_HOME/bin/sonar-scanner \
-            -Dsonar.projectKey=YouTube \
-            -Dsonar.sources=. \
-            -Dsonar.host.url=http://34.207.217.252:9000 \
-            -Dsonar.login=squ_9dea07319ad5853919897751e06e3570d1cee9d3
+  -Dsonar.projectKey=youtube \
+  -Dsonar.sources=. \
+  -Dsonar.host.url=http://34.207.217.252:9000 \
+  -Dsonar.token=squ_9dea07319ad5853919897751e06e3570d1cee9d3
         '''
     }
 }
